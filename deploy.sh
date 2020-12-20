0b535904d5c4690b05b72ac658e79bfa556bfb44
Alexander Shuvalov <ashuv_vyksa@mail.ru>
2020-12-20 22:52:55 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 if [[ $(docker ps | grep "$DOCKER_PROJECT" | awk '{print $1}') ]]; then docker ps | grep "$DOCKER_PROJECT" | awk '{print $1}' | xargs docker stop; fi
 if [[ $(docker ps -a | grep "$DOCKER_PROJECT" | awk '{print $1}') ]]; then docker ps -a | grep "$DOCKER_PROJECT" | awk '{print $1}' | xargs docker rm; fi
 if [[ $(docker images -a | grep "$DOCKER_PROJECT" | awk '{print $3}') ]]; then docker images -a | grep "$DOCKER_PROJECT" | awk '{print $3}' | xargs docker rmi; fi
-docker run -it --rm --network="host" -p "$PORT:$PORT" -v /home/duddits/portfolio.app/upload:/app/upload -d "$DOCKER_USER/$DOCKER_PROJECT"
+docker run -it --rm --network="host" -p "$PORT:$PORT" -v /home/duddits/portfolio.app/upload:/app/public/upload -d "$DOCKER_USER/$DOCKER_PROJECT"
