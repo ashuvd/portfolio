19a38137355a2ad46068dd7b409fa32d3bd1ce69
Alexander Shuvalov <ashuv_vyksa@mail.ru>
2020-12-20 14:38:42 +0300

    fix migrate settings

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,6 +3,14 @@ require('dotenv').config()
 module.exports = {
   development: {
     dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  },
+  production: {
+    dialect: 'postgres',
     host: "duddits.ru",
     port: +process.env.DB_PORT,
     username: process.env.DB_USER,
